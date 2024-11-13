925285af3ec3370cf088212bae5c2ca6068341f4
Bipon Roy <biponroy400@gmail.com>
2024-11-13 10:50:12 -0700

    added api for update order status

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,7 @@ import { OrderModel } from "../models/order.model";
 import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/apiError";
+import { isValidObjectId } from "mongoose";
 
 export const getOrderedItems = asyncHandler(async (req: Request, res: Response) => {
     const orders = await OrderModel.find({ userId: req.user._id }).sort("-createdAt");
@@ -11,4 +12,20 @@ export const getOrderedItems = asyncHandler(async (req: Request, res: Response) 
     }
 
     return res.status(200).json(new ApiResponse(200, orders, "All orders fetched successfully"));
+});
+
+export const updateOrderStatus = asyncHandler(async (req: Request, res: Response) => {
+    if (req.user?.role !== "admin") {
+        throw new ApiError(401, "Unauthorized Request");
+    }
+    const validStatus = ["delivered", "ordered", "shipped"];
+    const { orderId, deliveryStatus } = await req.body();
+
+    if (!isValidObjectId(orderId) || !validStatus.includes(deliveryStatus)) {
+        throw new ApiError(401, "Invalid Data");
+    }
+
+    await OrderModel.findByIdAndUpdate(orderId, { deliveryStatus });
+
+    return res.status(200).json(new ApiResponse(200, "Order status updated"));
 });
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware";
-import { getOrderedItems } from "../controllers/order.controller";
+import { getOrderedItems, updateOrderStatus } from "../controllers/order.controller";
 
 const router = Router();
 
 router.route("/all").get(verifyToken, getOrderedItems);
-
+router.route("/update-status").get(verifyToken, updateOrderStatus);
 export default router;
