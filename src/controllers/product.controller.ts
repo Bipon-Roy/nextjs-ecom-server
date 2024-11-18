d1afa6c29c5bc4702d6948324316439d9e549b4b
Bipon Roy <biponroy400@gmail.com>
2024-11-18 04:54:49 -0700

    added product review apis

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,6 +3,9 @@ import { ProductModel } from "../models/product.model";
 import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
+import { ReviewRequestType } from "../types";
+import { isValidObjectId, Types } from "mongoose";
+import { ReviewModel } from "../models/review.model";
 
 export const getAllProducts = asyncHandler(async (req: Request, res: Response) => {
     const products = await ProductModel.find();
@@ -36,4 +39,51 @@ export const getProductByCategory = asyncHandler(async (req: Request, res: Respo
     return res.status(200).json(new ApiResponse(200, products, `Products fetched successfully`));
 });
 
-export const addProductReviews = asyncHandler(async (req: Request, res: Response) => {});
+export const addProductReviews = asyncHandler(async (req: Request, res: Response) => {
+    const { productId, comment, rating } = req.body as ReviewRequestType;
+
+    if ([productId, comment].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "ProductId or comment fields are required");
+    }
+
+    if (!rating) {
+        throw new ApiError(400, `Rating is required`);
+    }
+
+    // Validate productId format
+    if (!isValidObjectId(productId)) {
+        throw new ApiError(400, `Invalid product ID`);
+    }
+
+    // Validate rating range
+    if (rating <= 0 || rating > 5) {
+        throw new ApiError(400, `Invalid rating value. Must be between 1 and 5.`);
+    }
+
+    const userId = req.user._id;
+    const data = { userId, rating, comment, product: productId };
+
+    // Upsert the review
+    await ReviewModel.findOneAndUpdate({ userId, product: productId }, data, {
+        upsert: true,
+    });
+
+    // Update the product's average rating
+    const [result] = await ReviewModel.aggregate([
+        { $match: { product: new Types.ObjectId(productId) } },
+        {
+            $group: {
+                _id: null,
+                averageRating: { $avg: "$rating" },
+            },
+        },
+    ]);
+
+    if (result?.averageRating) {
+        await ProductModel.findByIdAndUpdate(productId, {
+            rating: result.averageRating,
+        });
+    }
+
+    return res.status(201).json(new ApiResponse(201, "Thanks for your feedback!"));
+});
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { getAllProducts, getProductByCategory, getProductById } from "../controllers/product.controller";
+import {
+    addProductReviews,
+    getAllProducts,
+    getProductByCategory,
+    getProductById,
+} from "../controllers/product.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.route("/all").get(getAllProducts);
 router.route("/:id").get(getProductById);
 router.route("/category/:category").get(getProductByCategory);
+router.route("/add-review").post(verifyToken, addProductReviews);
 
 export default router;
