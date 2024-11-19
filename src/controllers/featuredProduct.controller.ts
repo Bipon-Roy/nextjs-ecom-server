eac328e019d31457eb980594576c22c0344b7ac4
Bipon Roy <biponroy400@gmail.com>
2024-11-19 23:45:50 +0600

    added delete api - FeaturedProduct

--- src/controllers/featuredProduct.controller.ts
+++ src/controllers/featuredProduct.controller.ts
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { FeaturedProductModel } from "../models/featuredProduct.model";
+import { removeImageFromCloud } from "../utils/cloudinary";
 
 export const getAllProducts = asyncHandler(async (req: Request, res: Response) => {
     const products = await FeaturedProductModel.find();
@@ -24,4 +25,24 @@ export const getProductById = asyncHandler(async (req: Request, res: Response) =
     }
 
     return res.status(200).json(new ApiResponse(200, product, "Product fetched successfully"));
+});
+
+export const deleteFeaturedProduct = asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    // Find the product by ID
+    const product = await FeaturedProductModel.findById(id);
+    if (!product) {
+        throw new ApiError(404, "Featured product not found");
+    }
+
+    // If the product has an associated image, delete it from Cloudinary
+    if (product && product.banner?.id) {
+        await removeImageFromCloud(product.banner.id);
+    }
+
+    // Delete the product from the database
+    await product.deleteOne();
+
+    return res.status(200).json(new ApiResponse(200, null, "Featured product deleted successfully"));
 });
--- src/routes/featuredProduct.routes.ts
+++ src/routes/featuredProduct.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllProducts, getProductById } from "../controllers/featuredProduct.controller";
+import { deleteFeaturedProduct, getAllProducts, getProductById } from "../controllers/featuredProduct.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.route("/all").get(getAllProducts);
 router.route("/:id").get(getProductById);
+router.route("/:id").delete(verifyToken, deleteFeaturedProduct);
 
 export default router;
