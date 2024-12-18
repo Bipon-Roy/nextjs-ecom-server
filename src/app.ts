c286021889228876b49e1280413219e19717e41e
Bipon Roy <biponroy400@gmail.com>
2024-12-18 03:58:35 -0700

    added notFound middleware for routes that doesn't exist

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import cartRoutes from "./routes/cart.routes";
 import wishlistRoutes from "./routes/wishlist.routes";
 import orderRoutes from "./routes/order.routes";
 import { errorHandler } from "./middlewares/errorHandler";
+import { notFoundHandler } from "./middlewares/notFound.middleware";
 const app = express();
 
 // Middleware
@@ -26,6 +27,9 @@ app.use("/api/v1/featured-products", featuredProductRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/wishlist", wishlistRoutes);
 app.use("/api/v1/orders", orderRoutes);
+
+// Handle 404 errors (non-existent routes)
+app.use(notFoundHandler);
 
 // Error handling middleware
 app.use(errorHandler);
